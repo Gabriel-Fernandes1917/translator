a7197e016b10559178650457ec18607069af7d05
Gabriel Fernandes <gabgui2001@gmail.com>
2021-08-06 19:11:01 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function play(){
+
+    var inpu;
+    var outp;
+
+    var inpu = document.getElementById('input').Value;
+
+    document.querySelector('#output').innerText = 'oi <p> oi <p> esse';
+    
+
+
+
+}
