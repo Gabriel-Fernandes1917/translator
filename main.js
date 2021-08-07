d8d8db1b6fe5bfa00fa9c247bc77e26446b87872
Gabriel Fernandes <gabgui2001@gmail.com>
2021-08-07 16:45:50 -0300

    adding more commands

--- main.js
+++ main.js
@@ -3,31 +3,24 @@ function research(){
     var x = document.getElementById('input').value;
   
     var word = x.toUpperCase();  // toUpperCase() deixa tudo maiusculo.
-    alert(word);
+    console.log(word);
+    //alert(word);
 
-    if(word == "VAR"){
-
-        document.querySelector('#output').innerHTML = "&nbsp;";
-        document.querySelector('#output').innerText = '>int nome = valor; para numeros inteiros <\n> float nome = valor; para numeros nao inteiros <\n> char[quantidade] nome = caracteres; para caracteres <\n> string nome = string; para string';
-
-    }else
-    if(word == "LET"){
+    if(word == "VAR" || word == "LET"){
 
         document.querySelector('#output').innerHTML = "&nbsp;";
         document.querySelector('#output').innerText = '>int nome = valor; para numeros inteiros <\n> float nome = valor; para numeros nao inteiros <\n> char[quantidade] nome = caracteres; para caracteres <\n> string nome = string; para string';
 
     }else
-    if(word == "DOCUMENT.WRITE"){
+    if(word == "DOCUMENT.WRITE" || word == "WRITE"){
         
         document.querySelector('#output').innerHTML = "&nbsp;";
         document.querySelector('#output').innerText = '> cout::std << "seu texto"; < \n > cout::std << "seu text" << endl; para quebrar linha apos o texto <';
     
     }else
-    if(word == "WRITE"){
-        
+    if(word == "LOOP" || word == "WHILE" || word == "FOR"){
         document.querySelector('#output').innerHTML = "&nbsp;";
-        document.querySelector('#output').innerText = '> cout::std << "seu texto"; < \n > cout::std << "seu text" << endl; para quebrar linha apos o texto <';
-    
+        document.querySelector('#output').innerText = '> while(condicao) < \n > for(int i; condicao; i++)';
     }
     else{
         document.querySelector('#output').innerHTML = "&nbsp;";
