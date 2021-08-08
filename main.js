da00ac4fa7881bac9a2fbbeca0ca85aac3a9411f
Gabriel Fernandes <gabgui2001@gmail.com>
2021-08-08 13:12:48 -0300

    add more comands

--- main.js
+++ main.js
@@ -9,18 +9,26 @@ function research(){
     if(word == "VAR" || word == "LET"){
 
         document.querySelector('#output').innerHTML = "&nbsp;";
-        document.querySelector('#output').innerText = '>int nome = valor; para numeros inteiros <\n> float nome = valor; para numeros nao inteiros <\n> char[quantidade] nome = caracteres; para caracteres <\n> string nome = string; para string';
+        document.querySelector('#output').innerText = 'int nome = valor; para numeros inteiros \n float nome = valor; para numeros nao inteiros \n char[quantidade] nome = caracteres; para caracteres \n string nome = string; para string';
 
     }else
     if(word == "DOCUMENT.WRITE" || word == "WRITE"){
         
         document.querySelector('#output').innerHTML = "&nbsp;";
-        document.querySelector('#output').innerText = '> cout::std << "seu texto"; < \n > cout::std << "seu text" << endl; para quebrar linha apos o texto <';
+        document.querySelector('#output').innerText = ' cout::std << "seu texto";  \n  cout::std << "seu text" << endl; para quebrar linha apos o texto ';
     
     }else
     if(word == "LOOP" || word == "WHILE" || word == "FOR"){
         document.querySelector('#output').innerHTML = "&nbsp;";
-        document.querySelector('#output').innerText = '> while(condicao) < \n > for(int i; condicao; i++)';
+        document.querySelector('#output').innerText = ' while(condicao) \n  for(int i; condicao; i++)';
+    }else
+    if(word == "GETELEMENTBYID" || word == "QUERYSELECTOR" || word == "GETELEMENTBYID()" || word == "INPUT" || word == "QUERYSELECTOR()"){
+        document.querySelector('#output').innerHTML = "&nbsp;";
+        document.querySelector('#output').innerText = ' cin >> variavel;';
+    }else
+    if(word == "FUNCTION" || word == "FUNCTION()") {
+        document.querySelector('#output').innerHTML = "&nbsp;";
+        document.querySelector('#output').innerText = 'function(){ }';
     }
     else{
         document.querySelector('#output').innerHTML = "&nbsp;";
@@ -56,3 +64,16 @@ function Write(){
 
 
 }*/
+
+
+// conversor
+
+function conversor(){
+
+
+    var name = "var nome = 'jose'";
+    alert(name.replace("var", "char"));
+
+    //alert(relace.);
+
+}
